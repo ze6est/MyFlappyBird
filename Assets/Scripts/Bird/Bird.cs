30cb680f5da2b09d4a7ad92fef94806c7f28b40b
Roman <ze6est1@yandex.ru>
2022-09-19 11:49:51 +0300

    main gameplay of game is done

---
+++ Assets/Scripts/Bird/Bird.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BirdMover))]
+public class Bird : MonoBehaviour
+{
+    private BirdMover _mover;
+    private int _score;
+
+    public event UnityAction GameOver;
+    public event UnityAction<int> ScoreChanged;
+
+    private void Start()
+    {
+        _mover = GetComponent<BirdMover>();
+    }
+
+    public void ResetPlayer()
+    {
+        _score = 0;
+        ScoreChanged?.Invoke(_score);
+        _mover.ResetBird();
+    }
+
+    public void IncreaseScore()
+    {
+        _score++;
+        ScoreChanged?.Invoke(_score);
+    }
+
+    public void Die()
+    {
+        GameOver?.Invoke();
+    }
+}
